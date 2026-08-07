9e2f53e8c279dcd8ed1f5b81d2f8d0c9a86fb456
Jonathan Yen <jyen@dvlt.ai>
2026-08-06 21:22:12 -0400

    chore: generate Xcode project with iOS and watchOS targets

---
+++ Watch/MelonTapWatchApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct MelonTapWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Melon Tap")
+        }
+    }
+}
---
+++ iOS/MelonTapApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct MelonTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Melon Tap")
+        }
+    }
+}
